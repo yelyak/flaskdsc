eb6880d7599e64625ad7e9a2e1d7d530b46a7648
yelyak <kayley@hklyfe.com>
2025-03-16 15:31:11 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -35,5 +35,29 @@ def scmp():
     }
     return render_template("scmp.html", news_dict=news_dict)
 
+@app.route("/nike")
+def nike():
+    return render_template("nike.html")
+
+@app.route("/nike/<string:category>")
+def nike_cat(category):
+    url = "https://www.nike.com.hk/" + category + "/shoe/list.htm?intpromo=PNTP"
+    soup_page = soup(requests.get(url).content, "html")
+
+    product_list_content = soup_page.find_all("dl", {"class" : "product_list_content"})
+    product_name_list = []
+    product_price_list = []
+
+    for product in product_list_content:
+        product_name_list.append(product.find("span", {"class" : "up"}).text)
+        product_price_list.append(product.find("dd", {"class" : "color666"}).text.split("HK$")[-1].replace(",", ""))
+
+    nike_dict = {
+        "names" : product_name_list,
+        "prices" : product_price_list
+    }
+    
+    return render_template("nikef.html", nike_dict=nike_dict, title=category)
+
 if __name__ == "__main__":
     app.run(debug=True)
